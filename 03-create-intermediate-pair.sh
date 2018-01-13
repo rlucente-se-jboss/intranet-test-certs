076f27abe9e15ce983197c10af779af8ec297697
rlucente <rlucente@redhat.com>
2018-01-13 00:37:55 -0500

    add SAN/UPN for client

--- 03-create-intermediate-pair.sh
+++ 03-create-intermediate-pair.sh
@@ -25,6 +25,9 @@ IP.1 = 127.0.0.1
 
 END1
 
+sed -i "s/CLIENT_SAN_HERE/otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$CLIENT_UPN/g" \
+    /root/ca/intermediate/openssl.conf
+
 # create the intermediate key
 cd /root/ca
 openssl genrsa -aes256 \
