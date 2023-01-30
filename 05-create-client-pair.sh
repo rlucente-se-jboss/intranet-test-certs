d75f0e7044d1d32bcbd0a3c7f3765955da42345b
rlucente <rlucente@redhat.com>
2023-01-30 16:21:59 -0500

    unique name for client cert/key

--- 05-create-client-pair.sh
+++ 05-create-client-pair.sh
@@ -20,15 +20,15 @@ sed -i.bak "s/\(otherName:msUPN;UTF8:\)..*/\1$CLIENT_UPN/g" \
 cd $WORKDIR/ca
 ${OPENSSL} genrsa -aes256 \
     -passout "$OPENSSL_DEFAULT_PASSWORD" \
-    -out intermediate/private/client.key.pem 2048
-chmod 400 intermediate/private/client.key.pem
+    -out intermediate/private/$CLIENT_USERNAME.key.pem 2048
+chmod 400 intermediate/private/$CLIENT_USERNAME.key.pem
 
 # create the cert
 
 ${OPENSSL} req -config intermediate/openssl.conf -new -sha256 \
     -passin "$OPENSSL_DEFAULT_PASSWORD" \
-    -key intermediate/private/client.key.pem \
-    -out intermediate/csr/client.csr.pem \
+    -key intermediate/private/$CLIENT_USERNAME.key.pem \
+    -out intermediate/csr/$CLIENT_USERNAME.csr.pem \
     -subj "$SUBJECT_CLIENT"
 
 # sign the cert with the intermediate ca
@@ -36,15 +36,15 @@ ${OPENSSL} req -config intermediate/openssl.conf -new -sha256 \
 ${OPENSSL} ca -config intermediate/openssl.conf \
     -batch -passin "$OPENSSL_DEFAULT_PASSWORD" \
     -extensions usr_cert -days 375 -notext -md sha256 \
-    -in intermediate/csr/client.csr.pem \
-    -out intermediate/certs/client.cert.pem
-chmod 444 intermediate/certs/client.cert.pem
+    -in intermediate/csr/$CLIENT_USERNAME.csr.pem \
+    -out intermediate/certs/$CLIENT_USERNAME.cert.pem
+chmod 444 intermediate/certs/$CLIENT_USERNAME.cert.pem
 
 # verify the certificate
 
 ${OPENSSL} x509 -noout -text \
-    -in intermediate/certs/client.cert.pem
+    -in intermediate/certs/$CLIENT_USERNAME.cert.pem
 
 ${OPENSSL} verify -CAfile intermediate/certs/ca-chain.cert.pem \
-    intermediate/certs/client.cert.pem
+    intermediate/certs/$CLIENT_USERNAME.cert.pem
 
