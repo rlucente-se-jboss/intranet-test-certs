c42452527506180aa1562e14ccbbf6851767b146
rlucente <rlucente@redhat.com>
2023-01-31 16:28:42 -0500

    add ability to use single self-signed CA

--- 03-create-intermediate-pair.sh
+++ 03-create-intermediate-pair.sh
@@ -8,6 +8,9 @@ echo "          Running: $0"
 echo "************************************************************************"
 echo
 
+SINGLE_CA=
+echo $USE_CHAINED_CA | grep -qiE 'true|yes|1' || SINGLE_CA=true
+
 # create the intermediate pair
 
 # prepare the directory
@@ -48,32 +51,54 @@ ${OPENSSL} genrsa -aes256 \
    -out intermediate/private/intermediate.key.pem 4096
 chmod 400 intermediate/private/intermediate.key.pem
 
-# create the intermediate certificate
-${OPENSSL} req -config intermediate/openssl.conf -new -sha256 \
-    -passin "$OPENSSL_DEFAULT_PASSWORD" \
-    -key intermediate/private/intermediate.key.pem \
-    -out intermediate/csr/intermediate.csr.pem \
-    -subj "$SUBJECT_INTERMEDIATE_CA"
+if [ $SINGLE_CA ]
+then
+    # create self-signed CA
+    ${OPENSSL} req -config intermediate/openssl.conf -new -sha256 \
+        -passin "$OPENSSL_DEFAULT_PASSWORD" \
+        -key intermediate/private/intermediate.key.pem \
+        -x509 -days 7300 -extensions v3_intermediate_ca \
+        -out intermediate/certs/intermediate.cert.pem \
+        -subj "$SUBJECT_INTERMEDIATE_CA"
 
-${OPENSSL} ca -config openssl.conf -extensions v3_intermediate_ca \
-    -batch -passin "$OPENSSL_DEFAULT_PASSWORD" \
-    -days 3650 -notext -md sha256 \
-    -in intermediate/csr/intermediate.csr.pem \
-    -out intermediate/certs/intermediate.cert.pem
+    chmod 444 intermediate/certs/intermediate.cert.pem
 
-chmod 444 intermediate/certs/intermediate.cert.pem
-
-# verify the intermediate certificate
-${OPENSSL} x509 -noout -text \
-    -in intermediate/certs/intermediate.cert.pem
-${OPENSSL} verify -CAfile certs/ca.cert.pem \
-    intermediate/certs/intermediate.cert.pem
-
-# the certificate chain file is the intermediate certificate
-# concatenated with the root CA
-cat intermediate/certs/intermediate.cert.pem \
-    certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
-chmod 444 intermediate/certs/ca-chain.cert.pem
+    # verify the self-signed CA
+    ${OPENSSL} x509 -noout -text \
+        -in intermediate/certs/intermediate.cert.pem
+    
+    # the certificate chain file is just the intermediate CA
+    cp intermediate/certs/intermediate.cert.pem \
+        intermediate/certs/ca-chain.cert.pem
+    chmod 444 intermediate/certs/ca-chain.cert.pem
+else
+    # create the intermediate certificate
+    ${OPENSSL} req -config intermediate/openssl.conf -new -sha256 \
+        -passin "$OPENSSL_DEFAULT_PASSWORD" \
+        -key intermediate/private/intermediate.key.pem \
+        -out intermediate/csr/intermediate.csr.pem \
+        -subj "$SUBJECT_INTERMEDIATE_CA"
+    
+    ${OPENSSL} ca -config openssl.conf -extensions v3_intermediate_ca \
+        -batch -passin "$OPENSSL_DEFAULT_PASSWORD" \
+        -days 3650 -notext -md sha256 \
+        -in intermediate/csr/intermediate.csr.pem \
+        -out intermediate/certs/intermediate.cert.pem
+    
+    chmod 444 intermediate/certs/intermediate.cert.pem
+    
+    # verify the intermediate certificate
+    ${OPENSSL} x509 -noout -text \
+        -in intermediate/certs/intermediate.cert.pem
+    ${OPENSSL} verify -CAfile certs/ca.cert.pem \
+        intermediate/certs/intermediate.cert.pem
+    
+    # the certificate chain file is the intermediate certificate
+    # concatenated with the root CA
+    cat intermediate/certs/intermediate.cert.pem \
+        certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
+    chmod 444 intermediate/certs/ca-chain.cert.pem
+fi
 
 # create the empty CRL
 ${OPENSSL} ca -config intermediate/openssl.conf \
